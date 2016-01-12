24143668e1e4511e40377557bf3d74e5def269fc
kibrom <kibmit@gmail.com>
2016-01-11 21:08:51 -0600

    Department class implemented.

--- src/lesson1/Department.java
+++ src/lesson1/Department.java
@@ -1,5 +1,20 @@
 package lesson1;
 
 public class Department {
+	
+	private String name;
+	
+	public Double getTotalSalary(){
+		
+		return 0.0;
+	}
+	
+	public void showAllMembers(){
+		
+	}
+	public int unitsPerFaculty(){
+		
+		return 0;
+	}
 
 }
