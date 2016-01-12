2ad67c11aea6ab66a1106811cb4158bfccc943c3
kibrom <kibmit@gmail.com>
2016-01-11 23:22:39 -0600

    Department application and staffStudent classes added

---
+++ src/lesson1/StaffStudet.java
@@ -0,0 +1,10 @@
+package lesson1;
+
+public class StaffStudet extends Staff {
+
+	public StaffStudet(String name, String phone, int age, double salary) {
+		super(name, phone, age, salary);
+		// TODO Auto-generated constructor stub
+	}
+
+}
